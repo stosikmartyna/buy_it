5ffad10bec8d304063fd4a6a4c32cc92cf8c9160
stosikmartyna <stosik.martyna@gmail.com>
2020-12-12 16:12:23 +0100

    Store input values in component state and display it

--- components/ShoppingList/ShoppingList.jsx
+++ components/ShoppingList/ShoppingList.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableHighlight, Button, StyleSheet } from 'react-native';
 
 export const ShoppingList = () => {
+    const [product, setProduct] = useState('');
+    const [listOfProducts, setListOfProducts] = useState([]);
+
+    const handleInputChange = (value) => {
+        setProduct(value)
+    }
+
+    const handleProductSubmit = () => {
+        setListOfProducts([...listOfProducts, product]);
+    }
     return (
         <View style={styles.container}>
-            <Text>ShoppingList</Text>
+            <Text style={styles.header}>Shopping List</Text>
+            <TextInput 
+                placeholder="ex. Milk" 
+                style={styles.input}
+                onChangeText={handleInputChange}
+            />
+            <TouchableHighlight style={styles.button}>
+                <Button color='#689FEF' title={'Add'} onPress={handleProductSubmit} />
+            </TouchableHighlight>
+            {listOfProducts.map((product, index) => {
+                return (
+                    <View style={styles.product} key={index}>
+                        <Text>{product}</Text>
+                    </View>
+                )
+            })}
         </View>
     )
 }
@@ -15,6 +40,35 @@ export const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+      },
+
+      header: {
+        color: '#689FEF',
+        fontSize: 30,
+        marginBottom: 10,
+      },
+
+      input: {
+        borderColor: '#474948', 
+        borderRadius: 4,
+        borderWidth: 1, 
+        marginBottom: 10,
+        padding: 10,
+        width: 260, 
+      },
+
+      button: {
+          marginBottom: 20
+      },
+
+      product: {
+        backgroundColor: '#d4e4fc',
+        borderColor: '#689FEF',
+        borderRadius: 4,
+        borderWidth: 1,
+        marginBottom: 10,
+        padding: 10,
+        width: 260,
       }
 })
     
