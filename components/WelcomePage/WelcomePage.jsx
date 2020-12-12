f5e5f5216c655fdc0f0fc67934406cacb80ed395
stosikmartyna <stosik.martyna@gmail.com>
2020-12-12 15:01:04 +0100

    Create WelcomePage

---
+++ components/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Image, Text, View, Button } from 'react-native';
+import { styles } from './WelcomePage.styles';
+
+export const WelcomePage = () => {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../.././assets/shopping.jpg')}/>
+            <Text style={styles.logoFirstWord}>
+                Buy
+                <Text style={styles.logoSecondWord}>
+                    It!
+                </Text>
+            </Text>
+            <Text style={styles.subtitle}>Shopping List App</Text>
+            <Button color='#689FEF' title='Start'/>
+        </View>
+    )
+}
---
+++ components/WelcomePage/WelcomePage.styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  
+    image: {
+      height: 260,
+      width: 346,
+    },
+  
+    logoFirstWord: {
+      color: '#689FEF',
+      fontSize: 60,
+      marginBottom: 10
+    },
+  
+    logoSecondWord: {
+      color: '#76A668'
+    },
+  
+    subtitle: {
+      color: '#474948',
+      marginBottom: 10,
+    },
+  });
