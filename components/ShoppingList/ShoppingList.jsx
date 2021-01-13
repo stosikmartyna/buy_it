d4980df1335fe94d927ba5f8ac8a2c6d7289687d
stosikmartyna <stosik.martyna@gmail.com>
2021-01-13 15:30:33 +0100

    Prevent swipe product when is add to basket

--- components/ShoppingList/ShoppingList.jsx
+++ components/ShoppingList/ShoppingList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
-import { SwipeListView } from 'react-native-swipe-list-view';
+import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
 import { useAuth } from '../../hooks/useAuth';
 import { useProducts } from '../../hooks/useProducts';
 import { styles } from './ShoppingList.styles';
@@ -46,30 +46,32 @@ export const ShoppingList = () => {
                 data={listOfProducts}
                 keyExtractor={(item => item.itemKey)}
                 renderItem={({item}) => (
-                    <TouchableHighlight  
-                        style={item.isInBasket ? styles.productInBasket : styles.product}
-                        underlayColor={'#bfd3f2'}
-                        id={item.itemKey} 
-                        onPress={() => addToBasket(item.itemKey)}
-                    >    
-                        <>
-                            <Text style={styles.productName}>{item.itemValue}</Text>
-                            {!item.isInBasket && <Image style={styles.shoppingListImage} source={require('../.././assets/basket.png')} />}
-                            {item.isInBasket && <Image style={styles.shoppingListImage} source={require('../.././assets/inBasket.png')} />}
-                        </>
-                    </TouchableHighlight >
+                    <SwipeRow 
+                        leftOpenValue={50} 
+                        disableLeftSwipe 
+                        disableRightSwipe={item.isInBasket === true}
+                    >
+                        <View>
+                            {!item.isInBasket && (
+                                <TouchableOpacity style={styles.hiddenItem} onPress={() => removeUserProduct(item.itemKey)}>
+                                    <Image style={styles.hiddenItemImage} source={require('../.././assets/bin.png')} />
+                                </TouchableOpacity>
+                            )}                       
+                        </View> 
+                        <TouchableHighlight  
+                            style={item.isInBasket ? styles.productInBasket : styles.product}
+                            underlayColor={item.isInBasket ? '#f9f9f9' :'#bfd3f2'}
+                            id={item.itemKey} 
+                            onPress={() => addToBasket(item.itemKey)}
+                        >    
+                            <>
+                                <Text style={styles.productName}>{item.itemValue}</Text>
+                                {!item.isInBasket && <Image style={styles.shoppingListImage} source={require('../.././assets/basket.png')} />}
+                                {item.isInBasket && <Image style={styles.shoppingListImage} source={require('../.././assets/inBasket.png')} />}
+                            </>
+                        </TouchableHighlight>
+                    </SwipeRow>
                 )}
-                renderHiddenItem={({item}) => (
-                    <View>
-                        {!item.isInBasket && (
-                            <TouchableOpacity style={styles.hiddenItem} onPress={() => removeUserProduct(item.itemKey)}>
-                                <Image style={styles.hiddenItemImage} source={require('../.././assets/bin.png')} />
-                            </TouchableOpacity>
-                        )}                       
-                    </View>    
-                )}
-                leftOpenValue={50}
-                disableLeftSwipe
             />
             <TouchableHighlight style={styles.signOutButton}>
                 <Button color={'#689FEF'} title={'Sign out'} onPress={signOut} style={styles.button} />
